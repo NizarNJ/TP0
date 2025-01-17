848ff7a692e3aefc0132f7916d5ba96f554f45fc
NizarNJ <nizarojamaoui@GMAIL.COM>
2025-01-17 09:57:45 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
 import math
 #TODO Affichez l'aire d'un cercle avec le rayon indiqué
 rayon = 4
+PI=3.14159
+aire=rayon*rayon*PI
+print("L'aire dun cercle de rayon",rayon,"Est",aire)
 
 #TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
+nom="Jamaoui"
+prenom="Nizar"
